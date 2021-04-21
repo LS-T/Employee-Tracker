07f72a9c1711fa718214e41a35f46a821ad8e251
LS-T <leo.s.teruel@gmail.com>
2021-04-20 22:05:22 -0400

    Updating employee functioning

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// Dependencies 
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-require('dotenv').config();
+require("dotenv").config();
 
+// Create connection to mySQL database
 const connection = mysql.createConnection({
   host: "localhost",
 
@@ -10,12 +12,14 @@ const connection = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
+
+// start function to ask the user what they would like to do next?
 const start = () => {
   inquirer
     .prompt({
       name: "action",
       type: "list",
-      message: "Would you like to do?",
+      message: " What would you like to do?",
       choices: [
         "View employees",
         "View departments",
@@ -35,6 +39,12 @@ const start = () => {
         case "View departments":
           viewDepartments();
           break;
+        case "Add a new department":
+          addDepartment();
+          break;
+        case "Update employee roles":
+          updateEmployeeRole();
+          break;
         case "exit":
           connection.end();
           break;
@@ -45,113 +55,114 @@ const start = () => {
     });
 };
 
+// function to view list of  current employees
 const viewEmployees = () => {
-  inquirer
-    .prompt({
-      name: "whichDepartment",
-      type: "list",
-      message: "Which department would you like to see the employees for?",
-      choices: ["Engineer", "Sales", "Logistics", "exit"],
-    })
-    .then((answer) => {
-      switch (answer.whichDepartment) {
-        case "Engineer":
-          let query1 =
-            "SELECT id, first_name, last_name FROM employee WHERE role_id=?";
-          connection.query(query1, ["2"], (err, results) => {
-            if (err) throw err;
-            results.forEach(({ id, first_name, last_name }) => {
-              console.log(
-                `id: ${id} || first-name: ${first_name} || last-name: ${last_name}`
-              );
-            });
-            viewEmployees();
-          });
-          break;
-        case "Sales":
-          let query2 =
-            "SELECT id, first_name, last_name FROM employee WHERE role_id=?";
-          connection.query(query2, ["1"], (err, results) => {
-            if (err) throw err;
-            results.forEach(({ id, first_name, last_name }) => {
-              console.log(
-                `id: ${id} || first-name: ${first_name} || last-name: ${last_name}`
-              );
-            });
-            viewEmployees();
-          });
-          break;
-        case "Logistics":
-          let query3 =
-            "SELECT id, first_name, last_name FROM employee WHERE role_id=?";
-          connection.query(query3, ["3"], (err, results) => {
-            if (err) throw err;
-            results.forEach(({ id, first_name, last_name }) => {
-              console.log(
-                `id: ${id} || first-name: ${first_name} || last-name: ${last_name}`
-              );
-            });
-            viewEmployees();
-          });
-          break;
-        case "exit":
-          start();
-          break;
-        default:
-          console.log("something wrong at view employee function");
-          break;
-      }
+  connection.query("SELECT * FROM employee", (err, res) => {
+    if (err) throw err;
+    res.map((employee) => {
+      console.log(employee.id, employee.first_name, employee.last_name);
+      return {
+        name: employee.first_name + " " + employee.last_name,
+        value: employee.id,
+      };
     });
+    start();
+  });
 };
-
+// function to view list of current departments 
 const viewDepartments = () => {
+  connection.query("SELECT * FROM departments", (err, res) => {
+    if (err) throw err;
+    res.map((department) => {
+      console.log(department.name);
+      return `${department.name}`;
+    });
+    start();
+  });
+};
+
+// function to add department to the table departments 
+const addDepartment = () => {
   inquirer
-    .prompt({
-      name: "department",
-      type: "list",
-      message: "Which department would you like to view?",
-      choices: ["Engineer", "Sales", "Logistics", "exit"],
-    })
+    .prompt([
+      {
+        name: "departmentName",
+        type: "input",
+        message: "What would you like to name the new department?",
+      },
+      {
+        name: "departmentID",
+        type: "input",
+        message: "What would you like the department ID to be?",
+      },
+    ])
     .then((answer) => {
-      switch (answer.department) {
-        case "Engineer":
-          let query1 = "SELECT name FROM departments WHERE id=?";
-          connection.query(query1, ["2"], (err, results) => {
-            if (err) throw err;
-            results.forEach(({ name }) => {
-              console.log(`name: ${name}`);
-            });
-          });
-          addDe
-          break;
-        case "Sales":
-          let query2 = "SELECT name FROM departments WHERE id=?";
-          connection.query(query2, ["1"], (err, results) => {
-            if (err) throw err;
-            results.forEach(({ id, name }) => {
-              console.log(`id: ${id} || name: ${name}`);
-            });
-          });
-          break;
-        case "Logistics":
-          let query3 = "SELECT id FROM departments WHERE name=?";
-          connection.query(query3, ["logistics"], (err, results) => {
-            if (err) throw err;
-            results.forEach(({ id, name }) => {
-              console.log(`id: ${id} || name: ${name}`);
-            });
-          });
-          break;
-        case "exit":
-          start();
-          break;
-        default:
-          console.log("something wrong at view employee function");
-          break;
-      }
+      connection.query(
+        "INSERT INTO departments SET ?",
+        {
+          name: answer.departmentName,
+          id: answer.departmentID,
+        },
+        (err) => {
+          if (err) throw err;
+          console.log("success");
+        }
+      );
+      start();
     });
 };
 
+// function to update employee role to a new role 
+const updateEmployeeRole = () => {
+  connection.query("SELECT * FROM employee", (err, res) => {
+    if (err) throw err;
+    let employeeData = res.map((employee) => {
+      console.log(employee.id, employee.first_name, employee.last_name);
+      return {
+        name: employee.first_name + " " + employee.last_name,
+        value: employee.id,
+      };
+    });
+    inquirer
+      .prompt({
+        name: "employeeID",
+        type: "list",
+        message: "Which employee would you like to update?",
+        choices: employeeData,
+      })
+      .then((answer) => {
+        connection.query("SELECT * FROM role", (err, res) => {
+          if (err) throw err;
+            //  set variable roleData to hold mapped results
+          let roleData = res.map((role) => {
+            return {
+              name: role.title,
+              value: role.id,
+            };
+          });
+          inquirer
+            .prompt({
+              name: "whichRole",
+              type: "list",
+              message: "Which role would you like to change to?",
+              choices: roleData,
+            })
+            .then((answer2) => {
+              connection.query(
+                "UPDATE employee SET role_id =? WHERE id =?",
+                [answer2.whichRole, answer.employeeID],
+                (err, res) => {
+                  if (err) throw err;
+                  console.log(res);
+                }
+              );
+            });
+        });
+      });
+  });
+};
+
+// setup connection to mySQL database
 connection.connect((err) => {
   if (err) throw err;
   start();
